4a5bcecc81827d6db9d8a55b60afa4be10f36b42
Simon Ekvy <simon.ekvy@gmail.com>
2013-01-11 17:47:34 +0100

    changes

--- android/LoanShark/src/com/sharks/loanshark/MainActivity.java
+++ android/LoanShark/src/com/sharks/loanshark/MainActivity.java
@@ -1,8 +1,13 @@
 package com.sharks.loanshark;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +15,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ListView listView = (ListView) findViewById(R.id.mylist);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		  "Linux", "OS/2" };
+
+		// Define a new Adapter
+		// First parameter - Context
+		// Second parameter - Layout for the row
+		// Third parameter - ID of the TextView to which the data is written
+		// Forth - the Array of data
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		  android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+		// Assign adapter to ListView
+		listView.setAdapter(adapter); 
+		
+		// Create a message handling object as an anonymous class.
+		OnItemClickListener mMessageClickedHandler = new OnItemClickListener() {
+		    public void onItemClick(AdapterView parent, View v, int position, long id) {
+		    	System.out.println("trolol");
+		        // Do something in response to the click
+		    }
+		};
+
+		listView.setOnItemClickListener(mMessageClickedHandler); 
+		
 	}
 
 	@Override
